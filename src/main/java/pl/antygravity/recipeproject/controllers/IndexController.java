13deb4c15fb59aec2c00bbc95f2599061ede7964
rawsk <rawski.andrzej@gmail.com>
2020-02-17 00:36:10 +0100

    Display List of Recipes on Index Page

--- src/main/java/pl/antygravity/recipeproject/controllers/IndexController.java
+++ src/main/java/pl/antygravity/recipeproject/controllers/IndexController.java
@@ -1,33 +1,23 @@
 package pl.antygravity.recipeproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.antygravity.recipeproject.domain.Category;
-import pl.antygravity.recipeproject.domain.UnitOfMeasure;
-import pl.antygravity.recipeproject.repositories.CategoryRepository;
-import pl.antygravity.recipeproject.repositories.UnitOfMeasureRepository;
-
-import java.util.Optional;
+import pl.antygravity.recipeproject.services.RecipeService;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
 
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("Cat Id is: " + categoryOptional.get().getId());
-        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
---
+++ src/main/java/pl/antygravity/recipeproject/services/RecipeServiceImpl.java
@@ -0,0 +1,25 @@
+package pl.antygravity.recipeproject.services;
+
+import org.springframework.stereotype.Service;
+import pl.antygravity.recipeproject.domain.Recipe;
+import pl.antygravity.recipeproject.repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+    }
+}

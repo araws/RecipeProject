ff89f115a8c26afa6eecc365862e46c236956a6e
rawsk <rawski.andrzej@gmail.com>
2020-02-29 21:20:21 +0100

    First JUnit tests

---
+++ src/test/java/pl/antygravity/recipeproject/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package pl.antygravity.recipeproject.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}

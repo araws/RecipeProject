21cfc80af27233bd0b1f092601c4e8d52e3d38d7
rawsk <rawski.andrzej@gmail.com>
2020-04-22 10:31:57 +0200

    Adding View for Ingredients

---
+++ src/main/java/pl/antygravity/recipeproject/controllers/IngredientController.java
@@ -0,0 +1,29 @@
+package pl.antygravity.recipeproject.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.antygravity.recipeproject.services.RecipeService;
+
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable String recipeId, Model model){
+        log.debug("Getting ingredient list for recipe id: " + recipeId);
+
+        //use command object to avoid lazy load errors in Thymeleaf.
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "recipe/ingredient/list";
+    }
+}

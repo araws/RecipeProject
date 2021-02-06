3ad69c2ea7d370a70ab01b04055e8147280f8785
Andrzej Rawski <rawski.andrzej@gmail.com>
2021-02-06 13:49:59 +0100

    Adding Display of Error Data

--- src/main/java/pl/antygravity/recipeproject/services/RecipeServiceImpl.java
+++ src/main/java/pl/antygravity/recipeproject/services/RecipeServiceImpl.java
@@ -41,7 +41,7 @@ public class RecipeServiceImpl implements RecipeService {
         Optional<Recipe> recipeOptional = recipeRepository.findById(l);
 
         if (recipeOptional.isEmpty()) {
-            throw new NotFoundException("Recipe Not Found");
+            throw new NotFoundException("Recipe Not Found. For ID value: " + l);
         }
         return recipeOptional.get();
     }

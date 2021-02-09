9efcc5b2d7014975f20a986fe10f61e3ca3e1f02
Andrzej Rawski <rawski.andrzej@gmail.com>
2021-02-09 08:31:58 +0100

    Displaying customised Validaiton Error Messages

--- src/test/java/pl/antygravity/recipeproject/controllers/RecipeControllerTest.java
+++ src/test/java/pl/antygravity/recipeproject/controllers/RecipeControllerTest.java
@@ -110,7 +110,9 @@ class RecipeControllerTest {
 
         mockMvc.perform(post("/recipe")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("id", ""))
+                .param("id", "")
+                .param("cookTime", "3000")
+        )
 
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("recipe"))
